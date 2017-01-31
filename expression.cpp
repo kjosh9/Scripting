13f7d298316d9f28804b59ad649fd60cdeafe38d
J. Knestaut <kjosh9@vt.edu>
2017-01-31 14:03:19 -0500

    created expression modules with function definitions required

---
+++ expression.cpp
@@ -0,0 +1,21 @@
+#include "expression.hpp"
+
+Expression::Expression(){
+
+}
+
+Expression::Expression(bool value){
+
+}
+
+Expression::Expression(double value){
+
+}
+
+Expression::Expression(const std::string & value){
+
+}
+
+bool Expression::operator==(const Expression & exp const noexcept{
+
+}
---
+++ expression.hpp
@@ -0,0 +1,24 @@
+#ifndef EXPRESSION_HPP
+#define EXPRESSION_HPP
+
+class Expression{
+
+public:
+	// Default construct and Expression of type None
+	Expression();
+
+	// Construct an Expression with a single Boolean atom with value
+	Expression(bool value);
+
+	// Construct and Expression with a single Number aton with value
+	Expression(double value);
+
+	// Construct an Expresion with a single Symbol atom with value
+	Expression(const std::string & value);
+
+	// Equality operator for two Expressions, to expression are equal if they have the same
+	// type, atom value, and number of arguments
+	bool operator==(const Expression & exp) const noexcept;
+
+};
+#endif // EXPRESSION_HPP
