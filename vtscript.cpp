98ca02fa1ca61bf1301ad748b361d40e89c9f92e
J. Knestaut <kjosh9@vt.edu>
2017-02-02 23:57:19 -0500

    created vtscript with basic filestream

---
+++ vtscript.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+
+#include "expression.hpp"
+#include "interpreter.hpp"
+#include "tokenize.hpp"
+
+int main(int agrc, char * argv[]){
+
+	//make a stack to handle command line arguments
+	//right now, we will assume there are no switches
+	
+
+	std::ifstream inFile(argv[1]);
+
+	while(inFile.is)open() && !inFile.eof()){
+					
+	}  	
+	
+
+	return EXIT_SUCCESS;
+
+}
