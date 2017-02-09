868f8cd0ef366731f92f44d2a05605b870405c74
J. Knestaut <kjosh9@vt.edu>
2017-02-09 13:27:33 -0500

    did some work on tokenize

--- tokenize.cpp
+++ tokenize.cpp
@@ -2,3 +2,14 @@
 
 
 
+// (+ a ( - 4 ))
+// "(", "+", "a", "(", "-", "4", ")", ")"
+
+bool TokenList::createList(std::sstream input){
+
+	while(!input.eof()){
+		input >> std::skipws >> ;
+	}
+}
+
+
--- tokenize.hpp
+++ tokenize.hpp
@@ -1,5 +1,34 @@
 #ifndef TOKENIZE_HPP
 #define TOKENIZE_HPP
 
+#include <string>
+#include <queue>
+#include <sstream>
+
+//do I actually need a class?
+class TokenList{
+
+
+public:
+	TokenList();
+
+	bool createList(std::sstream);
+
+	std::queue returnList();
+	
+
+private:
+
+	//the actual token list
+	std::queue<std::string> data;
+	
+};
+
+
+
+//Take in the input file string and "Tokenize" to be in the 
+// format that we expect. Return true if successful, false if
+// the string cannot be tokenized.
+
 
 #endif // TOKENIZE_HPP
